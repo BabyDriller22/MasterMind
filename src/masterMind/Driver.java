14bd56aac238827309fc97ad2929debcfc8a119f
BabyDriller22 <1226259@student.roc-nijmegen.nl>
2026-09-16 11:04:24 +0200

    Initial Commit

---
+++ src/masterMind/Driver.java
@@ -0,0 +1,48 @@
+package masterMind;
+
+public class Driver {
+
+	public static void main(String[] args) {
+//===================================		
+//		Codebreaker-pins
+		String green = "Green";
+		String yellow = "Yellow";
+		String red = "Red";
+		String purple = "Purple";
+		String blue = "Blue";
+		String orange = "Orange";
+		
+//		CodeBreaker-Row+Holes
+		String codeBreakerHole1 = "";
+		String codeBreakerHole2 = "";
+		String codeBreakerHole3 = "";
+		String codeBreakerHole4 = "";
+//===================================
+//		CodeMaker-pins
+		String black = "Black";
+		String white = "White";
+		
+//		CodeMaker-Row+Holes
+		String codeMakerHole1 = "";
+		String codeMakerHole2 = "";
+		String codeMakerHole3 = "";
+		String codeMakerHole4 = "";
+//		Secret-CodeField
+		String secretCodeField1 = "";
+		String secretCodeField2 = "";
+		String secretCodeField3 = "";
+		String secretCodeField4 = "";
+//===================================
+		int currentRow = 0;
+//===================================
+//		Game-begins
+		secretCodeField1 = green;
+		secretCodeField2 = yellow;
+		secretCodeField3 = red;
+		secretCodeField4 = blue;
+		
+		
+		
+	}
+
+}
